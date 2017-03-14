320de321a0d7b01b0ab90ecf9d89b5390f5e7118
Aaron Powell <abp168@msstate.edu>
2017-03-13 22:39:04 -0500

    adding client.cpp that reads through entire file on pluto through emulator. Still buggy however.

--- client.cpp
+++ client.cpp
@@ -12,6 +12,7 @@
 #include <fstream>
 #include <ctime>
 #include <time.h>
+#include <sys/poll.h>
 
 #include "packet.h"
 #include "packet.cpp"
@@ -60,9 +61,11 @@ int main (int argc, char ** argv){
 	int seqnum=0;
 	int ackseq=-1;
 	int window=0;
+	int send_base = 0;
 	int expectedack=0;
 	int location=0;
 	int timeout;
+	int interrupt;
 	
 	ifstream file;
 	file.open(argv[4]);
@@ -72,10 +75,14 @@ int main (int argc, char ** argv){
 	
 	ofstream seqnumfile;
 	seqnumfile.open("seqnum.log.txt");	
+	struct pollfd ufds[2]; // create poll struct to interrupt recvfrom()
+	ufds[0].fd = hostsocket;
+	ufds[0].events = POLLIN;
+	
 	
 	time_t start,stop;
 	
-	time(&start);	
+		
 
 	while (true) {	
 	
@@ -83,88 +90,137 @@ int main (int argc, char ** argv){
 		memset ((char*)&buffer,0,sizeof(buffer));
 		memset ((char*)&fbuffer,0,sizeof(fbuffer));
 		memset ((char*)&data,0,sizeof(data));
-		
-		if (window==7 || file.eof())
-		{		
-			//Recieves ackpacket from server
-			packet ackpacket(0,0,0,0);
-			recvfrom(hostsocket,data,sizeof(data),0,(struct sockaddr *)&emulator, &emulatorlen);		
-			ackpacket.deserialize((char*)data);
-			ackpacket.printContents();
-			ackseq=ackpacket.getSeqNum();
-			ackfile<<ackseq;
-			ackfile<<"\n";		
-//	
-				
-			time(&stop);
-			timeout=difftime(stop,start);
-//	
-				
-			if (expectedack!=ackseq || timeout>=2){
-				location= (expectedack-7) * 30;
-				file.seekg(location,ios::cur);
-				seqnum=expectedack;
-				window=0;	
-				
-//				
-				time(&start);											
-//
-					
-			}			
-			else{
-				
-			window--;
-			expectedack++;
-			if (expectedack==8){
-				expectedack=0;
-			}
 
-				if (window==0){
-					
-					//Makes and sends data EOF packet to server
-					type=3;	
-					packet endpacket(type,seqnum,0,0);
-					endpacket.serialize((char*) buffer);
-					sendto(hostsocket,buffer,sizeof(buffer),0,(struct sockaddr *)&emulator, sizeof(emulator));
-					endpacket.printContents();
-					seqnumfile<<seqnum;
-					seqnumfile<<"\n";
-	
-					//Recieves EOF ack from server
-					packet endackpacket(0,0,0,0);
-					recvfrom(hostsocket,data,sizeof(data),0,(struct sockaddr *)&emulator, &emulatorlen);		
-					endackpacket.deserialize((char*)data);
-					endackpacket.printContents();
-					ackseq=endackpacket.getSeqNum();
-					ackfile<<ackseq;
-					ackfile<<"\n";
-					break;
-				}
-			}
-		}
-		if (!file.eof()){
 		
-			file.read(fbuffer,30);
+		if (!file.eof())
+		  {
+		    file.read(fbuffer,30);
 			type=1;
-			if (seqnum==8){
+			if (seqnum==8)
+			  {
 				seqnum=0;
-			}
+			  }
 		
 			//Makes and sends data packet to server
 			packet datapacket(type,seqnum,sizeof(fbuffer),fbuffer);
 			datapacket.serialize((char*) buffer);
 			sendto(hostsocket,buffer,sizeof(buffer),0,(struct sockaddr *)&emulator, sizeof(emulator));
+			
 			datapacket.printContents();
 			seqnumfile<<seqnum;
 			seqnumfile<<"\n";
 						
 			seqnum++;
 			window++;
-		} 
+		  }
+	   
+		if (window==7)
+		  {
+		     interrupt= poll(ufds,1,2000);
+		    //Recieves ackpacket from server
+		    packet ackpacket(0,0,0,0);
+			
+			    
+		    if(interrupt == -1)
+		      {
+			printf("Error in polling.\n");
+		      }
+		    if (interrupt == 0)
+		      {
+			printf("Timeout occured!\n");
+		      }
+		    else
+		      {
+			if(ufds[0].revents & POLLIN)
+			{
+			    recvfrom(hostsocket,data,sizeof(data),0,(struct sockaddr *)&emulator, &emulatorlen);
+			    ackpacket.deserialize((char*)data);
+			    ackpacket.printContents();
+			    ackseq=ackpacket.getSeqNum();
+			    ackfile<<ackseq;
+			    ackfile<<"\n";
+		       	    if(send_base == ackseq)
+			      {
+				send_base++;
+				window =0;
+			      }
+			}
+		      }
+		  }
+			  
+			    /* if (expectedack!=ackseq) //|| timeout>=2)
+			      {
+				location= (expectedack-7) * 30;
+				file.seekg(location,ios::cur);
+				seqnum=expectedack;
+				window=0;	
+				
+				//				
+				//time(&start);											
+				//
+					
+			      }			
+			    else
+			      {
+				
+				window--;
+				expectedack++;
+				if (expectedack==8)
+				  {
+				    expectedack=0;
+				  }
 
-
-	}
+				  }*/
+		if ( file.eof())
+		  {
+					
+		    //Makes and sends data EOF packet to server
+		    type=3;	
+		    packet endpacket(type,seqnum,0,0);
+		    endpacket.serialize((char*) buffer);
+		    sendto(hostsocket,buffer,sizeof(buffer),0,(struct sockaddr *)&emulator, sizeof(emulator));
+		    endpacket.printContents();
+		    seqnumfile<<seqnum;
+		    seqnumfile<<"\n";
 	
+		    //Recieves EOF ack from server
+		    packet endackpacket(0,0,0,0);
+		    interrupt= poll(ufds,1,2000);
+		     if(interrupt == -1)
+		      {
+			printf("Error in polling.\n");
+		      }
+		    if (interrupt == 0)
+		      {
+			printf("Timeout occured!\n");
+		      }
+		    else
+		      {
+			if(ufds[0].revents & POLLIN)
+			{
+			    recvfrom(hostsocket,data,sizeof(data),0,(struct sockaddr *)&emulator, &emulatorlen);		
+			    endackpacket.deserialize((char*)data);
+			    endackpacket.printContents();
+			    ackseq=endackpacket.getSeqNum();
+			    ackfile<<ackseq;
+			    ackfile<<"\n";
+			    break;
+			}
+		      }
+		  }
+	}			
+		
+				
+//	
+				
+		/*time(&stop);
+		  timeout=difftime(stop,start);*/
+//	
+				
+			
+				
+
+
 
 	file.close();
 	ackfile.close();
